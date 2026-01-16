0dd64e1100c6428c325587dbb7ba174754e8ca07
Mohana2404 <mohanadharshini2404@gmail.com>
2026-01-16 18:09:47 +0530

    initial fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
 import './App.css'
-import Sidebar from './Sidebar.jsx'   
+
+import Home from './Home'
+
 
 
 const App = () => {
   return (
     <>
-    <div className='d-flex vh-100'>
-      <div className="w-20 "><Sidebar/></div>
-      <div className="w-80 bg-light">content</div>
+    <div className='vh-100'>
+      <Home />
+      
     </div>
-    
+   
     </>
-  )
-}
+  )}
 
 export default App
---
+++ src/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <nav className="navbar navbar-expand-lg bg-primary w-100 m-auto">
+  <div className="container-fluid">
+    <a className="navbar-brand text-white" href="#">TeleMed</a>
+    </div>
+    <div className="d-flex p-2">
+        <a className="nav-link text-white m-1 " href="#">Home</a>
+        <a className="nav-link text-white m-1 " href="#">About</a>
+        <a className="nav-link text-white m-1 " href="#">Contact</a>
+        <a className="nav-link text-white m-1 " href="#">Login</a>
+    </div>
+</nav></div>
+  )
+}
+
+export default Home
